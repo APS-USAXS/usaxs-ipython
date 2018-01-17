4054106682c44bc9568b61998a5bf225be2d5524
jemian <prjemian@gmail.com>
2018-01-17 13:55:07 -0600

    signals & imports

--- profile_bluesky/startup/20-detectors.py
+++ profile_bluesky/startup/20-detectors.py
@@ -1,12 +1,11 @@
 print(__file__)
 
-"""various detectors"""
+"""various detectors and other signals"""
 
 aps_current = EpicsSignalRO("S:SRcurrentAI", name="aps_current")
 
-## Beam Monitor Counts
-#bs_bm2 = EpicsSignalRO('BL14B:Det:BM2', name='bs_bm2')
-userCalc1_lax = EpicsSignalRO('9idcLAX:userCalc1', name='userCalc1_lax')
+userCalcs_lax = userCalcsDevice("9idcLAX:", name="userCalcs_lax")
+
 scaler0 = EpicsScaler('9idcLAX:vsc:c0', name='scaler0')
 scaler1 = EpicsScaler('9idcLAX:vsc:c1', name='scaler1')
 scaler2 = EpicsScaler('9idcLAX:vsc:c2', name='scaler2')
