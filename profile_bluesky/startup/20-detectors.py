1383bd164f109c14a3b44af9d37e92f19073eeca
jemian <prjemian@gmail.com>
2018-02-23 16:05:34 -0600

    watch the upstream scaler

--- profile_bluesky/startup/20-detectors.py
+++ profile_bluesky/startup/20-detectors.py
@@ -6,10 +6,6 @@ aps_current = EpicsSignalRO("S:SRcurrentAI", name="aps_current")
 
 userCalcs_lax = userCalcsDevice("9idcLAX:", name="userCalcs_lax")
 
-scaler0 = EpicsScaler('9idcLAX:vsc:c0', name='scaler0')
-scaler1 = EpicsScaler('9idcLAX:vsc:c1', name='scaler1')     # ? used
-scaler2 = EpicsScaler('9idcLAX:vsc:c2', name='scaler2')     # ? used
-
 """
 usaxs@usaxscontrol ~/.../startup/spec $ caget 9idcLAX:vsc:c{0,1,2}.NM{1,2,3,4,5,6,7,8}
 9idcLAX:vsc:c0.NM1             seconds
@@ -38,6 +34,10 @@ usaxs@usaxscontrol ~/.../startup/spec $ caget 9idcLAX:vsc:c{0,1,2}.NM{1,2,3,4,5,
 9idcLAX:vsc:c2.NM8             
 """
 
+# the old way
+# scaler0 = EpicsScaler('9idcLAX:vsc:c0', name='scaler0')
+# scaler1 = EpicsScaler('9idcLAX:vsc:c1', name='scaler1')     # used by softGlue for SAXS transmission
+# scaler2 = EpicsScaler('9idcLAX:vsc:c2', name='scaler2')     # used by upstream feedback
 
 scaler0 = ScalerCH('9idcLAX:vsc:c0', name='scaler0')
 # chan01 : sec (seconds)
@@ -47,3 +47,6 @@ scaler0 = ScalerCH('9idcLAX:vsc:c0', name='scaler0')
 # chan05 : trd (TR_diode)
 # chan06 : I000 (I000)
 scaler0.channels.read_attrs = ['chan01', 'chan02', 'chan03', 'chan04', 'chan05', 'chan06']
+# ignore scaler 1 for now
+scaler2_I000_counts = EpicsSignalRO("9idcLAX:vsc:c2.S2", name="scaler2_I000_counts")
+scaler2_I000_cps = EpicsSignalRO("9idcLAX:vsc:c2_cts1.B", name="scaler2_I000_counts")
