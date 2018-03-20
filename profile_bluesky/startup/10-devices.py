496906ea9e8723bc471abc23955eeff7ddc244b9
Service Account for Jan Ilavsky <usaxs@usaxscontrol.xray.aps.anl.gov>
2018-03-20 17:07:05 -0500

    support tuning

--- profile_bluesky/startup/10-devices.py
+++ profile_bluesky/startup/10-devices.py
@@ -7,7 +7,8 @@ from ophyd import PVPositioner, PVPositionerPC
 from ophyd.scaler import ScalerCH
 from APS_BlueSky_tools.devices import userCalcsDevice
 from APS_BlueSky_tools.devices import AxisTunerMixin
-
+from APS_BlueSky_tools.plans import TuneAxis
+from APS_BlueSky_tools.synApps_ophyd import swaitRecord
 
 # Set up custom or complex devices
 
