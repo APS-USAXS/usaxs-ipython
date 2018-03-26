fb3a7e12f92f8113c2a22c6fdb873bfdf0650843
Service Account for Jan Ilavsky <usaxs@usaxscontrol.xray.aps.anl.gov>
2018-03-26 14:33:19 -0500

    improve usability

--- profile_bluesky/startup/21-signals.py
+++ profile_bluesky/startup/21-signals.py
@@ -5,13 +5,14 @@ print(__file__)
 aps_current = EpicsSignalRO("S:SRcurrentAI", name="aps_current")
 
 mono_energy = EpicsSignal('9ida:BraggERdbkAO', name='mono_energy', write_pv="9ida:BraggEAO")
-und_us_energy = EpicsMotor('ID09us:Energy', name='und_us_energy', write_pv="ID09us:EnergySet")
-und_ds_energy = EpicsMotor('ID09ds:Energy', name='und_ds_energy', write_pv="ID09ds:EnergySet")
+und_us_energy = EpicsSignal('ID09us:Energy', name='und_us_energy', write_pv="ID09us:EnergySet")
+und_ds_energy = EpicsSignal('ID09ds:Energy', name='und_ds_energy', write_pv="ID09ds:EnergySet")
 
 
 userCalcs_lax = userCalcsDevice("9idcLAX:", name="userCalcs_lax")
 
 usaxs_q_calc = swaitRecord("9idcLAX:USAXS:Q", name="usaxs_q_calc")
+usaxs_q = usaxs_q_calc.val
 
 mr_val_center = EpicsSignal("9idcLAX:USAXS:MRcenter", name="mr_val_center")
 msr_val_center = EpicsSignal("9idcLAX:USAXS:MSRcenter", name="msr_val_center")
