95e571a75de67ca21aa6f6aa0fafb36da7068a81
Service Account for Jan Ilavsky <usaxs@usaxscontrol.xray.aps.anl.gov>
2018-04-09 14:10:17 -0500

    more AD config

--- profile_bluesky/startup/25-pilatus.py
+++ profile_bluesky/startup/25-pilatus.py
@@ -2,12 +2,19 @@ print(__file__)
 
 """Pilatus detector"""
 
-from ophyd import AreaDetector, PilatusDetectorCam
+from ophyd import AreaDetector, PilatusDetectorCam, PointGreyCam
 from ophyd import SingleTrigger, ImagePlugin, HDF5Plugin
 
 
+area_detector_EPICS_PV_prefix = {
+    'Pilatus 100k' : 'usaxs_pilatus1:',
+    'PointGrey BlackFly' : '9idFLY1:',
+    'Alta' : '9idalta:',
+}
+
+
 class MyPilatusCam(PilatusDetectorCam):
-    """custom detector support"""
+    """custom support for detector cam plugin"""
     # # FIXME: ophyd has problem with trying to unstage the RBV value inside RE()
     # image_mode = ADComponent(EpicsSignal, "ImageMode")
     # num_images = ADComponent(EpicsSignal, "NumImages")
@@ -17,7 +24,7 @@ class MyPilatusCam(PilatusDetectorCam):
 
 class MyHDF5Plugin(HDF5Plugin, FileStoreHDF5IterativeWrite):
 #class MyHDF5Plugin(HDF5Plugin):
-    """adapt HDF5 plugin for AD 2.5+"""
+    """custom support for detector HDF5 plugin (AD 2.5+)"""
     
     file_number_sync = None
     # FIXME:  .put() works OK but .value returns numpy object metadata
@@ -33,12 +40,32 @@ class MyHDF5Plugin(HDF5Plugin, FileStoreHDF5IterativeWrite):
 
 
 class MyPilatusDetector(SingleTrigger, AreaDetector):
-    """Pilatus detectors as used by 9-ID-C USAXS"""
+    """Pilatus detector(s) as used by 9-ID-C USAXS"""
     # TODO: configure the "root" and "write_path_template" attributes
-    # TODO: consider 100k & 300k separately?
+    # TODO: consider each detector separately?
     
     cam = ADComponent(MyPilatusCam, "cam1:")
     image = ADComponent(ImagePlugin, "image1:")
+    
+    # root : the path BlueSky can see
+    # write_path_template : the path the detector can see (starts with root)
+    hdf1 = ADComponent(
+        MyHDF5Plugin, 
+        "HDF1:", 
+        root="/",                   # TODO: root path for HDF5 files (for databroker filestore)
+        write_path_template="/tmp", # TODO: path for HDF5 files (for EPICS area detector)
+        reg=db.reg,
+
+
+class MyPointGreyDetector(SingleTrigger, AreaDetector):
+    """PointGrey Black Fly detector(s) as used by 9-ID-C USAXS"""
+    # TODO: configure the "root" and "write_path_template" attributes
+    # TODO: consider each detector separately?
+    
+    cam = ADComponent(PointGreyCam, "cam1:")
+    image = ADComponent(ImagePlugin, "image1:")
+    # root : the path BlueSky can see
+    # write_path_template : the path the detector can see (starts with root)
     hdf1 = ADComponent(
         MyHDF5Plugin, 
         "HDF1:", 
@@ -46,3 +73,7 @@ class MyPilatusDetector(SingleTrigger, AreaDetector):
         write_path_template="/tmp", # TODO: path for HDF5 files (for EPICS area detector)
         reg=db.reg,
         )
+
+
+# saxs_det = MyPilatusDetector(area_detector_EPICS_PV_prefix["Pilatus 100k"], name="saxs_det")
+# waxs_det = MyPointGreyDetector(area_detector_EPICS_PV_prefix["PointGrey BlackFly"], name="waxs_det")
