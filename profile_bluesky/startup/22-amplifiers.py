761ec7a33ab3df92f864dfcb9960e1a5c72efdc9
Service Account for Jan Ilavsky <usaxs@usaxscontrol.xray.aps.anl.gov>
2018-05-18 17:41:37 -0500

    #16 connect with all items in .ui screen

--- profile_bluesky/startup/22-amplifiers.py
+++ profile_bluesky/startup/22-amplifiers.py
@@ -2,15 +2,57 @@ print(__file__)
 
 """amplifiers"""
 
+from ophyd.device import DynamicDeviceComponent
+from ophyd.device import FormattedComponent
 
-class CurrentAmplifier(Device):
-	gain = Component(EpicsSignalRO, "gain")
+
+class CurrentAmplifierDevice(Device):
+    gain = Component(EpicsSignalRO, "gain")
 
 
-class FemtoAmplifier(CurrentAmplifier):
-	gainindex = Component(EpicsSignal, "gainidx")
-	description = Component(EpicsSignal, "femtodesc")
+class FemtoAmplifierDevice(CurrentAmplifierDevice):
+    gainindex = Component(EpicsSignal, "gainidx")
+    description = Component(EpicsSignal, "femtodesc")
 
-I0_femto = FemtoAmplifier('9idcUSX:fem02:seq01:', name='I0_femto')
-I00_femto = FemtoAmplifier('9idcUSX:fem03:seq01:', name='I00_femto')
-I000_femto = FemtoAmplifier('9idcUSX:fem04:seq01:', name='I000_femto')
+
+class DiodeRangeDevice(Device):
+    _default_configuration_attrs = ('gain', 'background', 'background_error')
+    _default_read_attrs = ('gain', 'background', 'background_error')
+
+    gain = FormattedComponent(EpicsSignal, '{self.prefix}gain{self._ch_num}')
+    background = FormattedComponent(EpicsSignal, '{self.prefix}bkg{self._ch_num}')
+    background_error = FormattedComponent(EpicsSignal, '{self.prefix}bkgErr{self._ch_num}')
+
+    def __init__(self, prefix, ch_num, **kwargs):
+        self._ch_num = ch_num
+
+        super().__init__(prefix, **kwargs)
+
+
+class DiodeControlsDevice(Device):
+    reqrange = Component(EpicsSignalRO, "reqrange")
+    mode = Component(EpicsSignal, "mode")
+    selected = Component(EpicsSignal, "selected")
+    gainU = Component(EpicsSignal, "gainU")
+    gainD = Component(EpicsSignal, "gainD")
+    range0 = Component(DiodeRangeDevice, '', ch_num=0)
+    range1 = Component(DiodeRangeDevice, '', ch_num=1)
+    range2 = Component(DiodeRangeDevice, '', ch_num=2)
+    range3 = Component(DiodeRangeDevice, '', ch_num=3)
+    range4 = Component(DiodeRangeDevice, '', ch_num=4)
+    counts_per_volt = Component(EpicsSignal, "vfc")
+    status = Component(EpicsSignalRO, "updating")
+    lurange = Component(EpicsSignalRO, "lurange")
+    lucounts = Component(EpicsSignalRO, "lucounts")
+    lurate = Component(EpicsSignalRO, "lurate")
+    lucurrent = Component(EpicsSignalRO, "lucurrent")
+    
+
+
+I_femto = FemtoAmplifierDevice('9idcUSX:fem01:seq01:', name='I_femto')
+I0_femto = FemtoAmplifierDevice('9idcUSX:fem02:seq01:', name='I0_femto')
+I00_femto = FemtoAmplifierDevice('9idcUSX:fem03:seq01:', name='I00_femto')
+I000_femto = FemtoAmplifierDevice('9idcUSX:fem04:seq01:', name='I000_femto')
+
+diode1_controls = DiodeControlsDevice("9idcUSX:pd01:seq01:", name="diode1_controls")
+diode2_controls = DiodeControlsDevice("9idcUSX:pd01:seq02:", name="diode2_controls")
