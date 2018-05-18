bf6e1c9b86872d26b1c6961028aaaa2e6e76cdb9
Service Account for Jan Ilavsky <usaxs@usaxscontrol.xray.aps.anl.gov>
2018-05-18 15:59:12 -0500

    common user support PVs

--- profile_bluesky/startup/21-signals.py
+++ profile_bluesky/startup/21-signals.py
@@ -23,3 +23,45 @@ mr_val_center = EpicsSignal("9idcLAX:USAXS:MRcenter", name="mr_val_center")
 msr_val_center = EpicsSignal("9idcLAX:USAXS:MSRcenter", name="msr_val_center")
 ar_val_center = EpicsSignal("9idcLAX:USAXS:ARcenter", name="ar_val_center")
 asr_val_center = EpicsSignal("9idcLAX:USAXS:ASRcenter", name="asr_val_center")
+
+
+class UserDataDevice(Device):
+    run_cycle = Component(EpicsSignal, "RunCycle")
+    user_name = Component(EpicsSignal, "UserName")
+    GUP_number = Component(EpicsSignal, "GUPNumber")
+    sample_title = Component(EpicsSignal, "USAXS:sampleTitle")
+    spec_file = Component(EpicsSignal, "USAXS:specFile")
+    sepc_scan = Component(EpicsSignal, "USAXS:specScan")
+    FS_order_number = Component(EpicsSignal, "USAXS:FS_OrderNumber")
+    user_dir = Component(EpicsSignal, "USAXS:userDir")
+    time_stamp = Component(EpicsSignal, "USAXS:timeStamp")
+    state = Component(EpicsSignal, "USAXS:state")
+
+user_data = UserDataDevice("9idcLAX:", name="user_data")
+
+mono_temperature = EpicsSignal("9ida:DP41:s1:temp", name="mono_temperature")
+cryo_level = EpicsSignal("9idCRYO:MainLevel:val", name="cryo_level")
+
+
+class APS_Machine_Parameters_Device(Device):
+    machine_status = Component(EpicsSignalRO, "S:DesiredMode")
+    operating_mode = Component(EpicsSignalRO, "S:ActualMode")
+    current = Component(EpicsSignalRO, "S:SRcurrentAI")
+    shutter_permit = Component(EpicsSignalRO, "ACIS:ShutterPermit")
+
+
+APS_machine_parameters = APS_Machine_Parameters_Device(name="APS_operator_messages")
+
+
+class APS_Operator_Messages_Device(Device):
+    operator = Component(EpicsSignalRO, "OPS:message1")
+    on_call = Component(EpicsSignalRO, "OPS:message2")
+    message3 = Component(EpicsSignalRO, "OPS:message3")
+    message4 = Component(EpicsSignalRO, "OPS:message4")
+    message5 = Component(EpicsSignalRO, "OPS:message5")
+    message6 = Component(EpicsSignalRO, "OPS:message6")
+    message7 = Component(EpicsSignalRO, "OPS:message7")
+    message8 = Component(EpicsSignalRO, "OPS:message8")
+
+
+APS_operator_messages = APS_Operator_Messages_Device(name="APS_operator_messages")
