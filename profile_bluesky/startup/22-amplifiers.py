618bade78a55d56b2c9a3bb2dab9ee567abe7962
Service Account for Jan Ilavsky <usaxs@usaxscontrol.xray.aps.anl.gov>
2018-05-21 14:22:31 -0500

    #16 wip : autoranging seq program setup needs a redesign

--- profile_bluesky/startup/22-amplifiers.py
+++ profile_bluesky/startup/22-amplifiers.py
@@ -30,18 +30,30 @@ class DiodeRangeDevice(Device):
 
 
 class AutorangeSettings(object):
-    """values allowed for ``DiodeControlsDevice.reqrange``"""
+    """values allowed for ``AmplifierSequenceControlsDevice.reqrange``"""
     automatic = "automatic"
     auto_background = "auto+background"
     manual = "manual"
 
 
-class DiodeControlsDevice(CurrentAmplifierDevice):
+class AmplifierSequenceControlsDevice(CurrentAmplifierDevice):
+    """
+    Ophyd support for amplifier sequence program
+    
+    TODO: resolve this question
+    Since the sequence program knows (from st.cmd) to which
+    amplifier and scaler it is using (and this configuration
+    is not available in any EPICS PVs), must we need 
+    coordinate that here as well?
+    """
     reqrange = Component(EpicsSignal, "reqrange")
     mode = Component(EpicsSignal, "mode")
     selected = Component(EpicsSignal, "selected")
     gainU = Component(EpicsSignal, "gainU")
     gainD = Component(EpicsSignal, "gainD")
+    # number of ranges dependent on amplifier, must be easily defined
+    # TODO: use DynamicDeviceComponent and supply range dict in constructor
+    # ranges = DynamicDeviceComponent(...)
     range0 = Component(DiodeRangeDevice, '', ch_num=0)
     range1 = Component(DiodeRangeDevice, '', ch_num=1)
     range2 = Component(DiodeRangeDevice, '', ch_num=2)
@@ -79,20 +91,34 @@ class DiodeControlsDevice(CurrentAmplifierDevice):
         return v
     
     def _decode_gain_target(self, target):
-        gain_list = (4, 6, 8, 10, 12)
-        assert target in gain_list, "`target` must be one of these values: {}".format(gain_list)
-        return gain_list.index(target)
-
-    def set_gain_plan(self, target):
         """
-        set gain on USAXS photodiode amplifier during a BlueSky plan
+        returns gain setting from requested converts ``target`` value
+        
+        Should override in subclass to customize for different 
+        amplifiers.  These are for USAXS photodiode.
         
         PARAMETERS
     
         target : int
             one of (4, 6, 8, 10, 12)
             corresponding, respectively, to gains of (1e4, 1e6, 1e8, 1e10, 1e12)
+
         
+        EXAMPLE CONTENT::
+
+            gain_list = (4, 6, 8, 10, 12)
+            err_msg = "supplied value ({}) not one of these: {}".format(
+                target, gain_list)
+            assert target in gain_list, err_msg
+            return gain_list.index(target)
+
+        """
+        raise NotImplementedError("Must define in subclass")
+
+    def set_gain_plan(self, target):
+        """
+        set gain on amplifier during a BlueSky plan
+
         Only use low noise gains; those are the only ones which actually work
         """
         yield from bps.abs_set(self.mode, AutorangeSettings.manual)
@@ -100,24 +126,62 @@ class DiodeControlsDevice(CurrentAmplifierDevice):
 
     def set_gain_cmd(self, target):
         """
-        set gain on USAXS photodiode amplifier directly, do not use during a BlueSky plan
-        
-        PARAMETERS
-    
-        target : int
-            one of (4, 6, 8, 10, 12)
-            corresponding, respectively, to gains of (1e4, 1e6, 1e8, 1e10, 1e12)
-        
+        set gain on amplifier directly, do not use during a BlueSky plan
+
         Only use low noise gains; those are the only ones which actually work
         """
         self.mode.put(AutorangeSettings.manual)
         self.reqrange.put(self._decode_gain_target(target))
 
 
-I_femto = FemtoAmplifierDevice('9idcUSX:fem01:seq01:', name='I_femto')
-I0_femto = FemtoAmplifierDevice('9idcUSX:fem02:seq01:', name='I0_femto')
-I00_femto = FemtoAmplifierDevice('9idcUSX:fem03:seq01:', name='I00_femto')
-I000_femto = FemtoAmplifierDevice('9idcUSX:fem04:seq01:', name='I000_femto')
+class UPD_AmplifierSequenceControlsDevice(AmplifierSequenceControlsDevice):
 
-femto200_controls = DiodeControlsDevice("9idcUSX:pd01:seq01:", name="femto200_controls")
-femto300_controls = DiodeControlsDevice("9idcUSX:pd01:seq02:", name="femto300_controls")
+    def _decode_gain_target(self, target):
+        """
+        converts ``target`` value to gain (index) value for USAXS photodiode
+        
+        PARAMETERS
+    
+        target : int
+            one of (4, 6, 8, 10, 12)
+            corresponding, respectively, to gains of (1e4, 1e6, 1e8, 1e10, 1e12)
+
+        """
+        gain_list = (4, 6, 8, 10, 12)
+        err_msg = "`target` must be one of these values: {}".format(gain_list)
+        assert target in gain_list, err_msg
+        return gain_list.index(target)
+
+
+class TRD_AmplifierSequenceControlsDevice(AmplifierSequenceControlsDevice):
+
+    def _decode_gain_target(self, target):
+        gain_list = (5, 6, 7, 8, 9, 10)
+        err_msg = "`target` must be one of these values: {}".format(gain_list)
+        assert target in gain_list, err_msg
+        if target < 10:
+            fmt = "1e{} low noise"
+        else:
+            fmt = "1e{} high speed"
+        return fmt.format(target)
+
+
+# use one or the other of these:
+#UPD_AMPLIFIER_ID = "DLPC200"
+UPD_AMPLIFIER_ID = "DLPCA300"
+
+UPD_femto  = FemtoAmplifierDevice('9idcUSX:fem01:seq01:', name='upd_femto')
+I0_femto   = FemtoAmplifierDevice('9idcUSX:fem02:seq01:', name='I0_femto')
+I00_femto  = FemtoAmplifierDevice('9idcUSX:fem03:seq01:', name='I00_femto')
+I000_femto = FemtoAmplifierDevice('9idcUSX:fem04:seq01:', name='I000_femto')
+trd_femto  = FemtoAmplifierDevice('9idcUSX:fem05:seq01:', name='trd_femto')
+
+UPD_seq = UPD_AmplifierSequenceControlsDevice(
+    dict(
+        DLPC200  = "9idcUSX:pd01:seq01:",
+        DLPCA300 = "9idcUSX:pd01:seq02:",
+    )[UPD_AMPLIFIER_ID],
+    name="UPD_seq")
+I0_seq  = AmplifierSequenceControlsDevice("9idcUSX:pd02:seq01:", name="I0_seq")
+I00_seq = AmplifierSequenceControlsDevice("9idcUSX:pd03:seq01:", name="I00_seq")
+TRD_seq = TRD_AmplifierSequenceControlsDevice("9idcUSX:pd05:seq01:", name="TRD_seq")
