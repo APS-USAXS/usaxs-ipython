52ef34a30492077868783172bda25e17357d5239
Service Account for Jan Ilavsky <usaxs@usaxscontrol.xray.aps.anl.gov>
2018-05-23 13:18:10 -0500

    #16: measures dark current backgrounds

--- profile_bluesky/startup/22-amplifiers.py
+++ profile_bluesky/startup/22-amplifiers.py
@@ -62,12 +62,11 @@ class FemtoAmplifierDevice(CurrentAmplifierDevice):
         self.acceptable_range_values = acceptable
 
         # assume gain labels are formatted "{float} {other_text}"
-        # and that other_text is the same for all
-        # TODO: we could verify that here!
         s = acceptable[0]
         self.gain_suffix = s[s.find(" "):]
         for i, s in enumerate(acceptable[:num_gains]):
-            msg = "acceptable[{}] = {}, expected to end with '{}'".format(i, s, self.gain_suffix)
+            # verify all gains use same suffix text
+            msg = "gainindex[{}] = {}, expected ending '{}'".format(i, s, self.gain_suffix)
             assert s[s.find(" "):] == self.gain_suffix
         
         self._gain_info_known = True
@@ -103,7 +102,7 @@ class FemtoAmplifierDevice(CurrentAmplifierDevice):
             raise ValueError(msg)
 
 
-class DiodeGainDevice(Device):
+class AmplfierGainDevice(Device):
     _default_configuration_attrs = ()
     _default_read_attrs = ('gain', 'background', 'background_error')
 
@@ -115,6 +114,34 @@ class DiodeGainDevice(Device):
         assert ch_num is not None, "Must provide `ch_num=` keyword argument."
         self._ch_num = ch_num
         super().__init__(prefix, **kwargs)
+    
+    def measure_background(self, parent, scaler, channel, numReadings):
+        assert numReadings > 0, "numReadings ({}) must be >0".format(numReadings)
+        assert isinstance(scaler, (EpicsScaler, ScalerCH))
+        assert isinstance(channel, EpicsSignalRO)
+        parent.setGain(self.gain.value) # use our own gain
+        scaler.preset_time.set(1.0)     # TODO: should be a parameter
+        readings = []
+        for i in range(numReadings):
+            scaler.count.set(1)     # start counting
+            time.sleep(0.01)        # wait to start
+            while scaler.count.value != 0:  # wait to complete
+                time.sleep(0.02)
+            readings.append(channel.value)
+        self.background.put(np.mean(readings))
+        self.background_error.put(np.std(readings))
+        print("-"*20)
+        print(self.gain.value)
+        print(readings)
+        print(self.background.value)
+        print(self.background_error.value)
+
+
+class AutorangeSettings(object):
+    """values allowed for sequence program's ``reqrange`` PV"""
+    automatic = "automatic"
+    auto_background = "auto+background"
+    manual = "manual"
 
 
 def _gains_subgroup_(cls, nm, suffix, gains, **kwargs):
@@ -137,7 +164,7 @@ class AmplifierAutoDevice(CurrentAmplifierDevice):
     gainD = Component(EpicsSignal, "gainD")
     ranges = DynamicDeviceComponent(
         _gains_subgroup_(
-            DiodeGainDevice, 'range', 'suffix', range(5)))
+            AmplfierGainDevice, 'gain', 'suffix', range(5)))
     counts_per_volt = Component(EpicsSignal, "vfc")
     status = Component(EpicsSignalRO, "updating")
     lurange = Component(EpicsSignalRO, "lurange")
@@ -169,22 +196,21 @@ class AmplifierAutoDevice(CurrentAmplifierDevice):
         self.acceptable_gain_values = acceptable
         
         # assume gain labels are formatted "{float} {other_text}"
-        # and that other_text is the same for all
-        # TODO: we could verify that here!
         s = acceptable[0]
         self.gain_suffix = s[s.find(" "):]
         for i, s in enumerate(acceptable[:num_gains]):
-            msg = "acceptable[{}] = {}, expected to end with '{}'".format(i, s, self.gain_suffix)
+            # verify all gains use same suffix text
+            msg = "reqrange[{}] = {}, expected ending: '{}'".format(i, s, self.gain_suffix)
             assert s[s.find(" "):] == self.gain_suffix
         
         self._gain_info_known = True
 
     def setGain(self, target):
         """
-        set the gain on the autorange controls
+        request the gain on the autorange controls
         
         Since the gain values are available from EPICS, 
-        we use that to provide a method that can set the 
+        we use that to provide a method that can request the 
         gain by any of these values:
         
         * gain text value (from EPICS)
@@ -193,7 +219,7 @@ class AmplifierAutoDevice(CurrentAmplifierDevice):
         
         Assumptions:
         
-        * gain label (from EPICS) is ALWAYS: "{float} {ignore this}"
+        * gain label (from EPICS) is ALWAYS: "{float} {self.gain_suffix}"
         * float mantissa is always one digit
         """
         if not self._gain_info_known:
@@ -212,10 +238,22 @@ class AmplifierAutoDevice(CurrentAmplifierDevice):
             msg += "must be one of these: {}".format(self.acceptable_gain_values)
             raise ValueError(msg)
 
-    def measure_dark_currents(self, scaler, numReadings=8):     # TODO: part of #16
+    def measure_dark_currents(self, scaler, detector, shutter, numReadings):
         """
+        measure the dark current background on each gain
+        
+        TODO: need to close/restore shutter
         """
-        pass
+        starting = dict(
+            gain = self.reqrange.value,
+            mode = self.mode.value,
+        )
+        self.mode.put(AutorangeSettings.manual)
+        for range_name in sorted(self.ranges.read_attrs):
+            gain = self.ranges.__getattr__(range_name)
+            gain.measure_background(self, scaler, detector, numReadings)
+        self.reqrange.put(starting["gain"])
+        self.mode.put(starting["mode"])
 
     def autoscale(self, scaler):                                # TODO: #16
         """
@@ -231,15 +269,20 @@ class AmplifierAutoDevice(CurrentAmplifierDevice):
 
 
 class DetectorAmplifierAutorangeDevice(Device):
-    scaler = FormattedComponent(EpicsSignal, '{self.scaler_channel_pv}')
+    scaler = FormattedComponent(EpicsScaler, '{self.scaler_pv}')
+    detector = FormattedComponent(EpicsSignalRO, '{self.scaler_pv}{self.detector_pv}')
     femto = FormattedComponent(FemtoAmplifierDevice, '{self.amplifier_pv}')
     auto = FormattedComponent(AmplifierAutoDevice, '{self.autorange_pv}')
     
-    def __init__(self, autorange_pv, scaler_channel_pv, amplifier_pv, **kwargs):
+    def __init__(self, scaler_pv, detector_pv, amplifier_pv, autorange_pv, **kwargs):
         self.autorange_pv = autorange_pv
-        self.scaler_channel_pv = scaler_channel_pv
+        self.scaler_pv = scaler_pv
+        self.detector_pv = detector_pv
         self.amplifier_pv = amplifier_pv
         super().__init__("", **kwargs)
+
+    def measure_dark_currents(self, shutter=None, numReadings=8):
+        self.auto.measure_dark_currents(self.scaler, self.detector, shutter, numReadings)
 
 
 # ------------
@@ -247,36 +290,40 @@ class DetectorAmplifierAutorangeDevice(Device):
 _amplifier_id_upd = epics.caget("9idcLAX:femto:model", as_string=True)
 
 upd_struct = DetectorAmplifierAutorangeDevice(
-    dict(
-            DLPCA200 = "9idcLAX:pd01:seq01:",
-            DDPCA300 = "9idcLAX:pd01:seq02:",
-        )[_amplifier_id_upd],
-    "9idcLAX:vsc:c0.S4",
+    "9idcLAX:vsc:c0",
+    ".S4",
     dict(
             DLPCA200 = "9idcLAX:fem01:seq01:",
             DDPCA300 = "9idcLAX:fem09:seq02:",
+        )[_amplifier_id_upd],
+    dict(
+            DLPCA200 = "9idcLAX:pd01:seq01:",
+            DDPCA300 = "9idcLAX:pd01:seq02:",
         )[_amplifier_id_upd],
     name="upd_struct",
 )
 
 trd_struct = DetectorAmplifierAutorangeDevice(
-    "9idcLAX:pd05:seq01:",
-    "9idcLAX:vsc:c0.S5",
+    "9idcLAX:vsc:c0",
+    ".S5",
     "9idcRIO:fem05:seq01:",
+    "9idcLAX:pd05:seq01:",
     name="trd_struct",
 )
 
 I0_struct = DetectorAmplifierAutorangeDevice(
-    "9idcLAX:pd02:seq01:",
-    "9idcLAX:vsc:c0.S2",
+    "9idcLAX:vsc:c0",
+    ".S2",
     "9idcRIO:fem02:seq01:",
+    "9idcLAX:pd02:seq01:",
     name="I0_struct",
 )
 
 I00_struct = DetectorAmplifierAutorangeDevice(
-    "9idcLAX:pd03:seq01:",
-    "9idcLAX:vsc:c0.S3",
+    "9idcLAX:vsc:c0",
+    ".S3",
     "9idcRIO:fem03:seq01:",
+    "9idcLAX:pd03:seq01:",
     name="I00_struct",
 )
 
