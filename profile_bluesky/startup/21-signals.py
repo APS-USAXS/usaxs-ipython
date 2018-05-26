9d413b668cb84d82193f9838edac4d9cdcf4f03b
Service Account for Jan Ilavsky <usaxs@usaxscontrol.xray.aps.anl.gov>
2018-05-26 16:11:24 -0500

    #20 there's more to it than this simple commit

--- profile_bluesky/startup/21-signals.py
+++ profile_bluesky/startup/21-signals.py
@@ -5,9 +5,21 @@ print(__file__)
 aps_current = EpicsSignalRO("S:SRcurrentAI", name="aps_current")
 
 FE_shutter = MyApsPssShutter("9ida:rShtrA", name="FE_shutter")
+# FE shutter state PV = PA:09ID:STA_A_FES_OPEN_PL.VAL
+
 mono_shutter = MyApsPssShutter("9ida:rShtrB", name="mono_shutter")
+# mono_shutter_state PV = PA:09ID:STA_B_SBS_OPEN_PL.VAL
+
 usaxs_shutter = InOutShutter("9idb:BioEnc2B3", name="usaxs_shutter")
+ti_filter_shutter = usaxs_shutter       # alias
+# a bit more complex to open ti_filter_shutter: open ALL blades
+
 ccd_shutter = InOutShutter("9idcRIO:Galil2Bo0_CMD", name="ccd_shutter")
+
+
+pf4_filter_bank_A = EpicsSignal('9idcRIO:pf4:fPosA', name='pf4_filter_bank_A')
+pf4_filter_bank_B = EpicsSignal('9idcRIO:pf4:fPosB', name='pf4_filter_bank_B')
+
 
 mono_energy = EpicsSignal('9ida:BraggERdbkAO', name='mono_energy', write_pv="9ida:BraggEAO")
 und_us_energy = EpicsSignal('ID09us:Energy', name='und_us_energy', write_pv="ID09us:EnergySet")
