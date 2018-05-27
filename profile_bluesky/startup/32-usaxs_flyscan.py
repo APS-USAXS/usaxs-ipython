d6dd97d74683e4ef43d71f4070f7fa4aed06f187
Pete Jemian <prjemian@gmail.com>
2018-05-26 19:40:23 -0500

    in case we want the thread symbol

--- profile_bluesky/startup/32-usaxs_flyscan.py
+++ profile_bluesky/startup/32-usaxs_flyscan.py
@@ -21,6 +21,7 @@ def run_in_thread(func):
     def wrapper(*args, **kwargs):
         thread = threading.Thread(target=func, args=args, kwargs=kwargs)
         thread.start()
+		return thread
     return wrapper
 
 
