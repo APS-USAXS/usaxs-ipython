685427e2022627a179d209bc4d743d27c57d5ffb
Service Account for Jan Ilavsky <usaxs@usaxscontrol.xray.aps.anl.gov>
2018-07-20 12:48:18 -0500

    #8 begins

--- profile_bluesky/startup/50-plans.py
+++ profile_bluesky/startup/50-plans.py
@@ -1,3 +1,12 @@
 print(__file__)
 
 # Bluesky plans (scans)
+
+
+def uascan():
+    """
+    USAXS step scan
+    
+    https://github.com/APS-USAXS/ipython-usaxs/issues/8
+    """
+    # TODO: needs proper args & kwargs matching SPEC's signature
