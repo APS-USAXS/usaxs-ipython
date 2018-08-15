1eaa9afba1912c5328a6d59b747c4f03a1e3230c
Pete Jemian <prjemian@gmail.com>
2018-08-14 21:52:22 -0500

    now in standard library

--- profile_bluesky/startup/42-procedure_registry.py
+++ profile_bluesky/startup/42-procedure_registry.py
@@ -1,5 +1,10 @@
 print(__file__)
 
+"""ProcedureRegistry is now in APS_BlueSky_tools.plans"""
+
+use_mode = APS_plans.ProcedureRegistry(name="ProcedureRegistry")
+
+
 """
 ProcedureRegistry for USAXS
 
@@ -47,100 +52,97 @@ EXAMPLE::
 
 """
 
-
-class ProcedureRegistry(Device):
-    """
-    Procedure Registry
-    
-    With many instruments, such as USAXS,, there are several operating 
-    modes to be used, each with its own setup code.  This ophyd Device
-    should coordinate those modes so that the setup procedures can be called
-    either as part of a Bluesky plan or from the command line directly.
-
-    Assumes that users will write functions to setup a particular 
-    operation or operating mode.  The user-written functions may not
-    be appropriate to use in a plan directly since they might
-    make blocking calls.  The ProcedureRegistry will call the function
-    in a thread (which is allowed to make blocking calls) and wait
-    for the thread to complete.
-    
-    It is assumed that each user-written function will not return until
-    it is complete.
-
-    .. autosummary::
-       
-        ~dir
-        ~add
-        ~remove
-        ~set
-        ~put
-
-    """
-    
-    busy = Component(Signal, value=False)
-    registry = {}
-    delay_s = 0
-    timeout_s = None
-    state = "__created__"
-    
-    @property
-    def dir(self):
-        """tuple of procedure names"""
-        return tuple(sorted(self.registry.keys()))
-    
-    def add(self, procedure, proc_name=None):
+if False:
+    class ProcedureRegistry(Device):
         """
-        add procedure to registry
-        """
-        #if procedure.__class__ == "function":
-        nm = proc_name or procedure.__name__
-        self.registry[nm] = procedure
-    
-    def remove(self, procedure):
-        """
-        remove procedure from registry
-        """
-        if isinstance(procedure, str):
-            nm = procedure
-        else:
-            nm = procedure.__name__
-        if nm in self.registry:
-            del self.registry[nm]
-    
-    def set(self, proc_name):
-        """
-        run procedure in a thread, return once it is complete
+        Procedure Registry
         
-        proc_name (str) : name of registered procedure to be run
+        With many instruments, such as USAXS,, there are several operating 
+        modes to be used, each with its own setup code.  This ophyd Device
+        should coordinate those modes so that the setup procedures can be called
+        either as part of a Bluesky plan or from the command line directly.
+
+        Assumes that users will write functions to setup a particular 
+        operation or operating mode.  The user-written functions may not
+        be appropriate to use in a plan directly since they might
+        make blocking calls.  The ProcedureRegistry will call the function
+        in a thread (which is allowed to make blocking calls) and wait
+        for the thread to complete.
+        
+        It is assumed that each user-written function will not return until
+        it is complete.
+
+        .. autosummary::
+           
+            ~dir
+            ~add
+            ~remove
+            ~set
+            ~put
+
         """
-        if not isinstance(proc_name, str):
-            raise ValueError("expected a procedure name, not {}".format(proc_name))
-        if proc_name not in self.registry:
-            raise KeyError("unknown procedure name: "+proc_name)
-        if self.busy.value:
-            raise RuntimeError("busy now")
- 
-        self.state = "__busy__"
-        status = DeviceStatus(self)
         
-        @APS_plans.run_in_thread
-        def run_and_delay():
-            self.busy.put(True)
-            self.registry[proc_name]()
-            # optional delay
-            if self.delay_s > 0:
-                time.sleep(self.delay_s)
-            self.busy.put(False)
-            status._finished(success=True)
+        busy = Component(Signal, value=False)
+        registry = {}
+        delay_s = 0
+        timeout_s = None
+        state = "__created__"
         
-        run_and_delay()
-        ophyd.status.wait(status, timeout=self.timeout_s)
-        self.state = proc_name
-        return status
-    
-    def put(self, value):   # TODO: risky?
-        """replaces ophyd Device default put() behavior"""
-        self.set(value)
-
-
-use_mode = ProcedureRegistry(name="ProcedureRegistry")
+        @property
+        def dir(self):
+            """tuple of procedure names"""
+            return tuple(sorted(self.registry.keys()))
+        
+        def add(self, procedure, proc_name=None):
+            """
+            add procedure to registry
+            """
+            #if procedure.__class__ == "function":
+            nm = proc_name or procedure.__name__
+            self.registry[nm] = procedure
+        
+        def remove(self, procedure):
+            """
+            remove procedure from registry
+            """
+            if isinstance(procedure, str):
+                nm = procedure
+            else:
+                nm = procedure.__name__
+            if nm in self.registry:
+                del self.registry[nm]
+        
+        def set(self, proc_name):
+            """
+            run procedure in a thread, return once it is complete
+            
+            proc_name (str) : name of registered procedure to be run
+            """
+            if not isinstance(proc_name, str):
+                raise ValueError("expected a procedure name, not {}".format(proc_name))
+            if proc_name not in self.registry:
+                raise KeyError("unknown procedure name: "+proc_name)
+            if self.busy.value:
+                raise RuntimeError("busy now")
+     
+            self.state = "__busy__"
+            status = DeviceStatus(self)
+            
+            @APS_plans.run_in_thread
+            def run_and_delay():
+                self.busy.put(True)
+                self.registry[proc_name]()
+                # optional delay
+                if self.delay_s > 0:
+                    time.sleep(self.delay_s)
+                self.busy.put(False)
+                status._finished(success=True)
+            
+            run_and_delay()
+            ophyd.status.wait(status, timeout=self.timeout_s)
+            self.state = proc_name
+            return status
+        
+        def put(self, value):   # TODO: risky?
+            """replaces ophyd Device default put() behavior"""
+            self.set(value)
