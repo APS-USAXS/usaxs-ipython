4c5e49fbb27ead2001635f4958b70147f57289d5
Service Account for Jan Ilavsky <usaxs@usaxscontrol.xray.aps.anl.gov>
2018-12-07 09:54:20 -0600

    fixes #82

--- profile_bluesky/startup/28-protection_plc.py
+++ profile_bluesky/startup/28-protection_plc.py
@@ -20,6 +20,16 @@ class PlcProtectionDevice(Device):
     
     SLEEP_POLL_s = 0.1
     _tripped_message = None
+
+    tripped_text = """
+    Equipment protection is engaged, no power on motors.
+    Fix PLC protection before any move. Stopping now.
+    Call beamline scientists if you do not understand.
+
+    !!!!!!  DO NOT TRY TO FIX THIS YOURSELF  !!!!!!
+
+    """
+    suspender = None
     
     @property
     def interlocked(self):
@@ -38,59 +48,83 @@ class PlcProtectionDevice(Device):
         if self.operations_status.value == 1:
             self._tripped_message = None
         else:
-            msg = """
-            Equipment protection is engaged, no power on motors.
-            Fix PLC protection before any move. Stopping now.
-            Call beamline scientists if you do not understand.
-            
-            !!!!!!  DO NOT TRY TO FIX THIS YOURSELF  !!!!!!
-            
-            """
+            msg = self.tripped_text
             if verbose:
                 print(msg)
             yield from bps.mv(
                 ti_filter_shutter, "close",
                 user_data.collection_in_progress, 0,     # notify the GUI and others
             )
+            if self.suspender is not None:
+                msg +="\n P.S. Can resume Bluesky scan: {}\n".format(
+                    suspender.allow_resume)
+
             # send email to staff ASAP!!!
             msg +="\n P.S. Can resume Bluesky scan: {}\n".format(
                 suspend_plc_protect.allow_resume)
             self._tripped_message = msg
             email_notices.send("!!! PLC protection Y0 tripped !!!", msg)
+    
+    def stop_in_suspender(self):
+        if self.operations_status.value == 1:
+            msg = None
+        else:
+            msg = self.tripped_text
+            if self.suspender is not None:
+                msg +="\n P.S. Can resume Bluesky scan: {}\n".format(
+                    self.suspender.allow_resume)
+            ti_filter_shutter.close()
+            user_data.collection_in_progress.put(0)     # notify the GUI and others
+        return msg
 
 
 class PlcProtectSuspendWhenChanged(APS_suspenders.SuspendWhenChanged):
+    """
+    Customize for PLC that protects against detector collisions
+    
+    Watch the PLC's Y0 bit that signals if the PLC internal checks 
+    are active and OK.  Suspend the RunEngine if this ever goes bad.
+    Force the user to quit the bluesky session and call the staff
+    to resolve the problem.
+    
+    See the simulation test here:
+    https://github.com/APS-USAXS/ipython-usaxs/blob/master/profile_bluesky/startup/notebooks/2018-12-05-USAXS-sim-plc-protect.ipynb
+    """
+    
+    justification_text = """. 
+    Significant equipment problem.  Do these steps:
+    1. ^C twice      # interrupt the ipython kernel
+    2. RE.abort()    # finalize current data streams (if any)
+    3. exit          # quit bluesky session
+    4. call beamline scientists
+
+    """
     
     def _get_justification(self):
         """override default method to call plc_protect.stop_if_tripped()"""
         if not self.tripped:
             return ''
 
+        self._tripped_message = None
         just = 'Signal {}, got "{}", expected "{}"'.format(
             self._sig.name,
             self._sig.get(),
             self.expected_value)
         if not self.allow_resume:
-            just += '.  '
-            just += '"RE.abort()" to finalize current data streams (if any)'
-            just += ' and then restart bluesky session to clear this.'
-            just += "\n"
-            just += "\n Equipment protection is engaged, no power on motors."
-            just += "\n Fix PLC protection before any move. Stopping now."
-            just += "\n Call beamline scientists if you do not understand."
-            just += "\n !!!!!!  DO NOT TRY TO FIX YOURSELF  !!!!!!\n"
+            just += self.justification_text
+            self._tripped_message = plc_protect.stop_in_suspender()
 
-        plc_protect.stop_if_tripped()       # TODO: test this
-
-        return ': '.join(s for s in (just, self._tripped_message)
-                         if s)
+        return '\n----\n'.join(
+            s for s in (just, self._tripped_message) if s)
 
 
 plc_protect = PlcProtectionDevice('9idcLAX:plc:', name='plc_protect')
 
+# Important for routine operations
+# see: https://github.com/APS-USAXS/ipython-usaxs/issues/82#issuecomment-444187217
 suspend_plc_protect = PlcProtectSuspendWhenChanged(
     plc_protect.operations_status, 
     expected_value=1)
-# this will suspend whenever PLC Y0 = 0 -- we want that
+# this will suspend whenever PLC Y0 = 0 ("not good") -- we want that!
 RE.install_suspender(suspend_plc_protect)
 
