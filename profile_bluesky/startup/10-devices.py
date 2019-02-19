0d5ae0def65404803b3749cb83eef9c53bbc34ee
Pete Jemian <prjemian@gmail.com>
2019-02-19 13:53:14 -0600

    #147 Struck3820 --> APS_devices.Struck3820

--- profile_bluesky/startup/10-devices.py
+++ profile_bluesky/startup/10-devices.py
@@ -23,44 +23,6 @@ class MyWaveform(Device):
 
 
 # move to APS_devices
-class Struck3820(Device):
-    """Struck/SIS 3820 Multi-Channel Scaler (as used by USAXS)"""
-    start_all = Component(EpicsSignal, "StartAll")
-    stop_all = Component(EpicsSignal, "StopAll")
-    erase_start = Component(EpicsSignal, "EraseStart")
-    erase_all = Component(EpicsSignal, "EraseAll")
-    mca1 = Component(EpicsMCARecord, "mca1")
-    mca2 = Component(EpicsMCARecord, "mca2")
-    mca3 = Component(EpicsMCARecord, "mca3")
-    # mca1Name = Component(EpicsSignal, "scaler1.NM1", string=True)
-    # mca2Name = Component(EpicsSignal, "scaler1.NM2", string=True)
-    # mca3Name = Component(EpicsSignal, "scaler1.NM3", string=True)
-    clock_frequency = Component(EpicsSignalRO, "clock_frequency")
-    current_channel = Component(EpicsSignalRO, "CurrentChannel")
-    channel_max = Component(EpicsSignalRO, "MaxChannels")
-    channels_used = Component(EpicsSignal, "NuseAll")
-    elapsed_real_time = Component(EpicsSignalRO, "ElapsedReal")
-    preset_real_time = Component(EpicsSignal, "PresetReal")
-    dwell_time = Component(EpicsSignal, "Dwell")
-    prescale = Component(EpicsSignal, "Prescale")
-    acquiring = Component(EpicsSignalRO, "Acquiring", string=True)
-    acquire_mode = Component(EpicsSignalRO, "AcquireMode", string=True)
-    model = Component(EpicsSignalRO, "Model", string=True)
-    firmware = Component(EpicsSignalRO, "Firmware")
-    channel_advance = Component(EpicsSignal, "ChannelAdvance")
-    count_on_start = Component(EpicsSignal, "CountOnStart")
-    channel_advance = Component(EpicsSignal, "SoftwareChannelAdvance")
-    channel1_source = Component(EpicsSignal, "Channel1Source")
-    user_led = Component(EpicsSignal, "UserLED")
-    mux_output = Component(EpicsSignal, "MUXOutput")
-    input_mode = Component(EpicsSignal, "InputMode")
-    output_mode = Component(EpicsSignal, "OutputMode")
-    output_polarity = Component(EpicsSignal, "OutputPolarity")
-    read_rate = Component(EpicsSignal, "ReadAll.SCAN")
-    do_readl_all = Component(EpicsSignal, "DoReadAll")
-
-
-# move to APS_devices
 class KohzuSeqCtl_Monochromator(Device):
     """
     synApps Kohzu double-crystal monochromator sequence control program
