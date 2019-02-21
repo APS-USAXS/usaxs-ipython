bbd8210a95de86c7cd455225ea4b699ab1d4c3c8
Pete Jemian <prjemian@gmail.com>
2019-02-21 12:16:49 -0600

    #149 simulate USAXS shutter at times

--- profile_bluesky/startup/21-filters_shutters.py
+++ profile_bluesky/startup/21-filters_shutters.py
@@ -13,6 +13,10 @@ if aps.inUserOperations and operations_in_9idc():
         "PA:09ID:STA_B_SBS_OPEN_PL.VAL",
         name="mono_shutter")
 
+    usaxs_shutter = APS_devices.EpicsOnOffShutter(
+        "9idcLAX:userTran3.A", 
+        name="usaxs_shutter")
+
 else:
     print("!"*30)
     print("Session started when APS not operating.")
@@ -20,12 +24,11 @@ else:
     print("!"*30)
     FE_shutter = SimulatedApsPssShutterWithStatus(name="FE_shutter")
     mono_shutter = SimulatedApsPssShutterWithStatus(name="mono_shutter")
+    usaxs_shutter = SimulatedApsPssShutterWithStatus(name="usaxs_shutter")
 
 
-usaxs_shutter = APS_devices.EpicsOnOffShutter("9idcLAX:userTran3.A", name="usaxs_shutter")
 ti_filter_shutter = usaxs_shutter       # alias
 ti_filter_shutter.delay_s = 0.2         # shutter needs some recovery time
-## a bit more complex to open ti_filter_shutter: open ALL blades
 
 
 ccd_shutter = APS_devices.EpicsOnOffShutter("9idcRIO:Galil2Bo0_CMD", name="ccd_shutter")
