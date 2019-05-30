83c94b831d2055be785f8e81483f816353db28c0
Pete Jemian <prjemian@gmail.com>
2019-05-30 12:29:15 -0500

    fixes #220

--- profile_bluesky/startup/50-plans.py
+++ profile_bluesky/startup/50-plans.py
@@ -475,6 +475,54 @@ def parse_Excel_command_file(filename):
     return commands
 
 
+def split_quoted_line(line):
+    """
+    splits a line into words some of which might be quoted
+    
+    TESTS::
+
+        FlyScan 0   0   0   blank
+        FlyScan 5   2   0   "empty container"
+        FlyScan 5   12   0   "even longer name"
+        SAXS 0 0 0 blank
+        SAXS 0 0 0 "blank"
+    
+    RESULTS::
+
+        ['FlyScan', '0', '0', '0', 'blank']
+        ['FlyScan', '5', '2', '0', 'empty container']
+        ['FlyScan', '5', '12', '0', 'even longer name']
+        ['SAXS', '0', '0', '0', 'blank']
+        ['SAXS', '0', '0', '0', 'blank']
+
+    """
+    parts = []
+
+    # look for open and close quoted parts and combine them
+    quoted = False
+    multi = None
+    for p in line.split():
+        if not quoted and p.startswith('"'):   # begin quoted text
+            quoted = True
+            multi = ""
+
+        if quoted:
+            if len(multi) > 0:
+                multi += " "
+            multi += p
+            if p.endswith('"'):     # end quoted text
+                quoted = False
+
+        if not quoted:
+            if multi is not None:
+                parts.append(multi[1:-1])   # remove enclosing quotes
+                multi = None
+            else:
+                parts.append(p)
+
+    return parts
+
+
 def parse_text_command_file(filename):
     """
     parse a text file with commands, return as command list
@@ -534,7 +582,7 @@ def parse_text_command_file(filename):
             continue                    # comment or blank
 
         else:                           # command line
-            action, *values = row.split()  # FIXME:
+            action, *values = split_quoted_line(row)
             commands.append((action, values, i+1, raw_command.rstrip()))
 
     return commands
