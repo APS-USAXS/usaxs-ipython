c67c708de911fb063dc6be82f9c56c7ee9ee01d5
Pete Jemian <prjemian@gmail.com>
2019-06-10 12:52:31 -0500

    ENH #150

--- profile_bluesky/startup/10-devices.py
+++ profile_bluesky/startup/10-devices.py
@@ -230,8 +230,13 @@ class UserDataDevice(Device):
 
 
 class PSS_Parameters(Device):
+    a_beam_active = Component(EpicsSignalRO, "PA:09ID:A_BEAM_ACTIVE.VAL", string=True)
+    b_beam_active = Component(EpicsSignalRO, "PA:09ID:B_BEAM_ACTIVE.VAL", string=True)
+    # does not connect: a_beam_ready = Component(EpicsSignalRO, "PA:09ID:A_BEAM_READY.VAL", string=True)
+    b_beam_ready = Component(EpicsSignalRO, "PA:09ID:B_BEAM_READY.VAL", string=True)
     c_shutter_closed_chain_A = Component(EpicsSignalRO, "PA:09ID:SCS_PS_CLSD_LS", string=True)
     c_shutter_closed_chain_B = Component(EpicsSignalRO, "PB:09ID:SCS_PS_CLSD_LS", string=True)
+    # other signals?
 
     @property
     def c_station_enabled(self):
@@ -255,10 +260,37 @@ class PSS_Parameters(Device):
 class BLEPS_Parameters(Device):
     """Beam Line Equipment Protection System"""
     # TODO: BLEPS main "OK" value 
+    red_light = Component(EpicsSignalRO, "9idBLEPS:RED_LIGHT", string=True)
+    station_shutter_b = Component(EpicsSignalRO, "9idBLEPS:SBS_CLOSED", string=True)
+    flow_1 = Component(EpicsSignal, "9idBLEPS:FLOW1_CURRENT", write_pv="9idBLEPS:FLOW1_SET_POINT", string=True)
+    flow_2 = Component(EpicsSignal, "9idBLEPS:FLOW2_CURRENT", write_pv="9idBLEPS:FLOW2_SET_POINT", string=True)
+    temperature_1_chopper = Component(EpicsSignal, "9idBLEPS:TEMP1_CURRENT", write_pv="9idBLEPS:TEMP1_SET_POINT", string=True)
+    temperature_2 = Component(EpicsSignal, "9idBLEPS:TEMP2_CURRENT", write_pv="9idBLEPS:TEMP2_SET_POINT", string=True)
+    temperature_3 = Component(EpicsSignal, "9idBLEPS:TEMP3_CURRENT", write_pv="9idBLEPS:TEMP3_SET_POINT", string=True)
+    temperature_4 = Component(EpicsSignal, "9idBLEPS:TEMP4_CURRENT", write_pv="9idBLEPS:TEMP4_SET_POINT", string=True)
+    temperature_5 = Component(EpicsSignal, "9idBLEPS:TEMP5_CURRENT", write_pv="9idBLEPS:TEMP5_SET_POINT", string=True)
+    temperature_6 = Component(EpicsSignal, "9idBLEPS:TEMP6_CURRENT", write_pv="9idBLEPS:TEMP6_SET_POINT", string=True)
+    temperature_7 = Component(EpicsSignal, "9idBLEPS:TEMP7_CURRENT", write_pv="9idBLEPS:TEMP7_SET_POINT", string=True)
+    temperature_8 = Component(EpicsSignal, "9idBLEPS:TEMP8_CURRENT", write_pv="9idBLEPS:TEMP8_SET_POINT", string=True)
+    # other signals?
+    
+    # technically, these come from the FE-EPS IOC, reading signals from the BL-EPS
+    shutter_permit = Component(EpicsSignalRO, "EPS:09:ID:BLEPS:SPER", string=True)
+    vacuum_permit = Component(EpicsSignalRO, "EPS:09:ID:BLEPS:VACPER", string=True)
+    vacuum_ok = Component(EpicsSignalRO, "EPS:09:ID:BLEPS:VAC", string=True)
 
 
 class FEEPS_Parameters(Device):
     """Front End Equipment Protection System"""
+    fe_permit = Component(EpicsSignalRO, "EPS:09:ID:FE:PERM", string=True)
+    major_fault = Component(EpicsSignalRO, "EPS:09:ID:Major", string=True)
+    minor_fault = Component(EpicsSignalRO, "EPS:09:ID:Minor", string=True)
+    mps_permit = Component(EpicsSignalRO, "EPS:09:ID:MPS:RF:PERM", string=True)
+    photon_shutter_1 = Component(EpicsSignalRO, "EPS:09:ID:PS1:POSITION", string=True)
+    photon_shutter_2 = Component(EpicsSignalRO, "EPS:09:ID:PS2:POSITION", string=True)
+    safety_shutter_1 = Component(EpicsSignalRO, "EPS:09:ID:SS1:POSITION", string=True)
+    safety_shutter_2 = Component(EpicsSignalRO, "EPS:09:ID:SS2:POSITION", string=True)
+    # other signals?
 
 
 # these are the global settings PVs for various parts of the instrument
@@ -510,12 +542,15 @@ class GeneralParameters(Device):
 
 class DiagnosticsParameters(Device):
     """for beam line diagnostics and post-mortem analyses"""
-    # TODO: 9idcLAX:blCalc:userCalc1 "BeamInHutch". 
-    # see https://github.com/APS-USAXS/ipython-usaxs/issues/150#issuecomment-499547981
+    beam_in_hutch_swait = Component(APS_synApps_ophyd.swaitRecord , "9idcLAX:blCalc:userCalc1")
 
     PSS = Component(PSS_Parameters)
     BL_EPS = Component(BLEPS_Parameters)
     FE_EPS = Component(FEEPS_Parameters)
+    
+    @property
+    def beam_in_hutch(self):
+        return self.beam_in_hutch_swait.val.value != 0
 
 
 class Linkam_CI94(APS_devices.ProcessController):
