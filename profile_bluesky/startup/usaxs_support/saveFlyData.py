6958d38633f56beffc50b61fc21ce7c4736a7895
Pete Jemian <prjemian@gmail.com>
2019-07-29 23:28:57 -0500

    WIP #276 most switchover is done

--- profile_bluesky/startup/usaxs_support/saveFlyData.py
+++ profile_bluesky/startup/usaxs_support/saveFlyData.py
@@ -26,186 +26,15 @@ import h5py
 
 # matches IOC for big arrays
 os.environ['EPICS_CA_MAX_ARRAY_BYTES'] = '1280000'    # was 200000000
-import epics                # PyEpics support
+from . import nexus
 
 
 path = os.path.dirname(__file__)
 XML_CONFIGURATION_FILE = os.path.join(path, 'saveFlyData.xml')
 XSD_SCHEMA_FILE = os.path.join(path, 'saveFlyData.xsd')
 
-# TODO: refactor
-# see: https://github.com/APS-USAXS/ipython-usaxs/issues/276#issuecomment-516027300
-field_registry = {}    # key: node/@label,        value: Field_Specification object
-group_registry = {}    # key: HDF5 absolute path, value: Group_Specification object
-link_registry = {}     # key: node/@label,        value: Link_Specification object
-pv_registry = {}       # key: node/@label,        value: PV_Specification object
-
 
 class TimeoutException(Exception): pass
-
-
-def getGroupObjectByXmlNode(xml_node):
-    '''locate a Group_Specification object by matching its xml_node'''
-    for group_spec_obj in group_registry.values():
-        if group_spec_obj.xml_node == xml_node:
-            return group_spec_obj
-    return None
-
-
-class Field_Specification(object):
-    '''specification of the "field" element in the XML configuration file'''
-
-    def __init__(self, xml_element_node):
-        self.xml_node = xml_element_node
-        xml_parent_node = xml_element_node.getparent()
-        self.group_parent = getGroupObjectByXmlNode(xml_parent_node)
-        self.name = xml_element_node.attrib['name']
-        self.hdf5_path = self.group_parent.hdf5_path + '/' + self.name
-
-        nodes = xml_element_node.xpath('text')
-        if len(nodes) > 0:
-            self.text = nodes[0].text.strip()
-        else:
-            self.text = ''
-
-        self.attrib = {}
-        for node in xml_element_node.xpath('attribute'):
-            self.attrib[node.attrib['name']] = node.attrib['value']
-
-        field_registry[self.hdf5_path] = self
-
-    def __str__(self):
-        try:
-            nm = self.hdf5_path
-        except Exception:
-            nm = 'Field_Specification object'
-        return nm
-
-
-class Group_Specification(object):
-    '''specification of the "group" element in the XML configuration file'''
-
-    def __init__(self, xml_element_node):
-        self.hdf5_path = None
-        self.xml_node = xml_element_node
-        self.hdf5_group = None
-        self.name = xml_element_node.attrib['name']
-        self.nx_class = xml_element_node.attrib['class']
-
-        self.attrib = {}
-        for node in xml_element_node.xpath('attribute'):
-            self.attrib[node.attrib['name']] = node.attrib['value']
-
-        xml_parent_node = xml_element_node.getparent()
-        self.group_children = {}
-        if xml_parent_node.tag == 'group':
-            # identify our parent
-            self.group_parent = getGroupObjectByXmlNode(xml_parent_node)
-            # next, find our HDF5 path from our parent
-            path = self.group_parent.hdf5_path
-            if not path.endswith('/'):
-                path += '/'
-            self.hdf5_path = path + self.name
-            # finally, declare ourself to be a child of that parent
-            self.group_parent.group_children[self.hdf5_path] = self
-        elif xml_parent_node.tag == 'NX_structure':
-            self.group_parent = None
-            self.hdf5_path = '/'
-        if self.hdf5_path in group_registry:
-            msg = "Cannot create duplicate HDF5 path names: path=%s name=%s nx_class=%s" % (
-                self.hdf5_path, self.name, self.nx_class)
-            raise RuntimeError(msg)
-        group_registry[self.hdf5_path] = self
-
-    def __str__(self):
-        return self.hdf5_path or 'Group_Specification object'
-
-
-class Link_Specification(object):
-    '''specification of the "link" element in the XML configuration file'''
-
-    def __init__(self, xml_element_node):
-        self.xml_node = xml_element_node
-
-        self.name = xml_element_node.attrib['name']
-        self.source_hdf5_path = xml_element_node.attrib['source']   # path to existing object
-        self.linktype = xml_element_node.get('linktype', 'NeXus')
-        if self.linktype not in ('NeXus', ):
-            msg = "Cannot create HDF5 " + self.linktype + " link: " + self.hdf5_path
-            raise RuntimeError(msg)
-
-        xml_parent_node = xml_element_node.getparent()
-        self.group_parent = getGroupObjectByXmlNode(xml_parent_node)
-        self.name = xml_element_node.attrib['name']
-        self.hdf5_path = self.group_parent.hdf5_path + '/' + self.name
-
-        link_registry[self.hdf5_path] = self
-
-    def make_link(self, hdf_file_object):
-        '''make this NeXus link within the HDF5 file'''
-        source = self.source_hdf5_path      # source: existing HDF5 object
-        parent = '/'.join(source.split('/')[0:-1])     # parent: parent HDF5 path of source
-        target = self.hdf5_path             # target: HDF5 node path to be created
-        parent_obj = hdf_file_object[parent]
-        source_obj = hdf_file_object[source]
-
-        str_source = str(source_obj.name).encode("utf-8")
-        str_target = target.encode("utf-8")
-        if 'target' not in source_obj.attrs:
-            # NeXus link, NOT an HDF5 link!
-            source_obj.attrs["target"] = str_source
-        parent_obj[str_target] = parent_obj[str_source]
-
-    def __str__(self):
-        try:
-            nm = self.label + ' <' + self.pvname + '>'
-        except Exception:
-            nm = 'Link_Specification object'
-        return nm
-
-
-class PV_Specification(object):
-    '''specification of the "PV" element in the XML configuration file'''
-
-    def __init__(self, xml_element_node):
-        self.xml_node = xml_element_node
-        self.label = xml_element_node.attrib['label']
-        if self.label in pv_registry:
-            msg = "Cannot use PV label more than once: " + self.label
-            raise RuntimeError(msg)
-        self.pvname = xml_element_node.attrib['pvname']
-        self.as_string = xml_element_node.attrib.get('string', "false").lower() in ('t', 'true')
-        # _s = xml_element_node.attrib.get('string', "false")
-        # print(f"PV: {self.pvname}  string:{self.as_string}  node:{_s}")
-        self.pv = None
-        aas = xml_element_node.attrib.get('acquire_after_scan', 'false')
-        self.acquire_after_scan = aas.lower() in ('t', 'true')
-
-        self.attrib = {}
-        for node in xml_element_node.xpath('attribute'):
-            self.attrib[node.attrib['name']] = node.attrib['value']
-
-        # identify our parent
-        xml_parent_node = xml_element_node.getparent()
-        self.group_parent = getGroupObjectByXmlNode(xml_parent_node)
-
-        self.length_limit = xml_element_node.get('length_limit', None)
-        if self.length_limit is not None:
-            if not self.length_limit.startswith('/'):
-                # convert local to absolute reference
-                self.length_limit = self.group_parent.hdf5_path + '/' + self.length_limit
-
-        # finally, declare ourself to be a child of that parent
-        self.hdf5_path = self.group_parent.hdf5_path + '/' + self.label
-        self.group_parent.group_children[self.hdf5_path] = self
-        pv_registry[self.hdf5_path] = self
-
-    def __str__(self):
-        try:
-            nm = self.label + ' <' + self.pvname + '>'
-        except Exception:
-            nm = 'PV_Specification object'
-        return nm
 
 
 class SaveFlyScan(object):
@@ -223,7 +52,18 @@ class SaveFlyScan(object):
 
         path = self._get_support_code_dir()
         self.config_file = config_file or os.path.join(path, XML_CONFIGURATION_FILE)
-        self._read_configuration()
+        
+        self.mgr = nexus.get_manager(self.config_file)
+        if not self.mgr.configured:
+            self.mgr._read_configuration()
+            t0 = time.time()
+            self.mgr._connect_ophyd()
+            for _i in range(50):   # limited wait to connect
+                verdict = self.mgr.connected
+                logger.debug(f"connected: {verdict}  time:{time.time()-t0}")
+                if verdict:
+                    break       # seems to take about 60-70 ms with current XML file
+                time.sleep(0.01)
         self._prepare_to_acquire()
 
     def waitForData(self):
@@ -234,6 +74,7 @@ class SaveFlyScan(object):
             #if not time_remains:
             #    raise TimeoutException()
             return not triggered
+        # FIXME: for bluesky ops and REMOVE calls to epics.
         self.trigger = epics.PV(self.trigger_pv)
         #timeout_s = max(0, epics.caget(self.scantime_pv))
         #quitting_time = datetime.datetime.now() + datetime.timedelta(seconds=(timeout_s+70))
@@ -261,21 +102,21 @@ class SaveFlyScan(object):
 
     def preliminaryWriteFile(self):
         '''write all preliminary data to the file while fly scan is running'''
-        for pv_spec in pv_registry.values():
+        for pv_spec in self.mgr.pv_registry.values():
             if pv_spec.acquire_after_scan:
                 continue
             if pv_spec.as_string:
-                value = pv_spec.pv.get(as_string=True)
+                value = pv_spec.ophyd_signal.get(as_string=True)
             else:
-                value = pv_spec.pv.get()
+                value = pv_spec.ophyd_signal.get()
             if value is None:
                 value = 'no data'
             logger.debug("saveFile(): writing {pv_spec}")
             if not isinstance(value, numpy.ndarray):
                 value = [value]
             else:
-                if pv_spec.length_limit and pv_spec.length_limit in pv_registry:
-                    length_limit = pv_registry[pv_spec.length_limit].pv.get()
+                if pv_spec.length_limit and pv_spec.length_limit in self.mgr.pv_registry:
+                    length_limit = self.mgr.pv_registry[pv_spec.length_limit].ophyd_signal.get()
                     if len(value) > length_limit:
                         value = value[:length_limit]
 
@@ -301,24 +142,24 @@ class SaveFlyScan(object):
         '''write all desired data to the file and exit this code'''
         t = datetime.datetime.now()
         timestamp = datetime.datetime.isoformat(t, sep=" ")
-        f = group_registry['/'].hdf5_group
+        f = self.mgr.group_registry['/'].hdf5_group
         f.attrs["timestamp"] = timestamp
 
         # note: len(caget(array)) returns NORD (number of useful data)
-        for pv_spec in pv_registry.values():
+        for pv_spec in self.mgr.pv_registry.values():
             if not pv_spec.acquire_after_scan:
                 continue
             if pv_spec.as_string:
-                value = pv_spec.pv.get(as_string=True)
+                value = pv_spec.ophyd_signal.get(as_string=True)
             else:
-                value = pv_spec.pv.get()
+                value = pv_spec.ophyd_signal.get()
             if value is None:
                 value = 'no data'
             if not isinstance(value, numpy.ndarray):
                 value = [value]
             else:
-                if pv_spec.length_limit and pv_spec.length_limit in pv_registry:
-                    length_limit = pv_registry[pv_spec.length_limit].pv.get()
+                if pv_spec.length_limit and pv_spec.length_limit in self.mgr.pv_registry:
+                    length_limit = self.mgr.pv_registry[pv_spec.length_limit].ophyd_signal.get()
                     if len(value) > length_limit:
                         value = value[:length_limit]
 
@@ -337,71 +178,11 @@ class SaveFlyScan(object):
                 #raise
 
         # as the final step, make all the links as directed
-        for _k, v in link_registry.items():
+        for _k, v in self.mgr.link_registry.items():
             v.make_link(f)
 
         f.close()    # be CERTAIN to close the file
         logger.debug("saveFile(): file closed")
-
-    def _read_configuration(self):
-        global field_registry, group_registry, link_registry, pv_registry
-
-        # first, validate configuration file against an XML Schema
-        path = self._get_support_code_dir()
-        xml_schema_file = os.path.join(path, XSD_SCHEMA_FILE)
-        xmlschema_doc = lxml_etree.parse(xml_schema_file)
-        xmlschema = lxml_etree.XMLSchema(xmlschema_doc)
-
-        config = lxml_etree.parse(self.config_file)
-        if not xmlschema.validate(config):
-            # XML file is not valid, let lxml report what is wrong as an exception
-            #log = xmlschema.error_log    # access more details
-            xmlschema.assertValid(config)   # basic exception report
-
-        # safe to proceed parsing the file
-        root = config.getroot()
-        if root.tag != "saveFlyData":
-            raise RuntimeError("XML file not valid for configuring saveFlyData")
-
-        self.creator_version = root.attrib['version']
-
-        node = root.xpath('/saveFlyData/triggerPV')[0]
-        self.trigger_pv = node.attrib['pvname']
-        acceptable_values = (int(node.attrib['done_value']), node.attrib['done_text'])
-        self.trigger_accepted_values = acceptable_values
-
-        node = root.xpath('/saveFlyData/timeoutPV')[0]
-        self.timeout_pv = node.attrib['pvname']
-
-        # initial default value set in this code
-        # pull default poll_interval_s from XML Schema (XSD) file
-        xsd_root = xmlschema_doc.getroot()
-        xsd_node = xsd_root.xpath("//xs:attribute[@name='poll_time_s']", # name="poll_time_s"
-                              namespaces={'xs': 'http://www.w3.org/2001/XMLSchema'})
-
-        # allow XML configuration to override trigger_poll_interval_s
-        default_value = float(xsd_node[0].get('default', self.trigger_poll_interval_s))
-        self.trigger_poll_interval_s = node.get('poll_time_s', default_value)
-
-        # reset these global variables
-        field_registry = {}    # key: node/@label,        value: Field_Specification object
-        group_registry = {}    # key: HDF5 absolute path, value: Group_Specification object
-        link_registry = {}     # key: node/@label,        value: Link_Specification object
-        pv_registry = {}       # key: node/@label,        value: PV_Specification object
-
-
-        nx_structure = root.xpath('/saveFlyData/NX_structure')[0]
-        for node in nx_structure.xpath('//group'):
-            Group_Specification(node)
-
-        for node in nx_structure.xpath('//field'):
-            Field_Specification(node)
-
-        for node in nx_structure.xpath('//PV'):
-            PV_Specification(node)
-
-        for node in nx_structure.xpath('//link'):
-            Link_Specification(node)
 
     def _get_support_code_dir(self):
         return os.path.split(os.path.abspath(__file__))[0]
@@ -409,11 +190,11 @@ class SaveFlyScan(object):
     def _prepare_to_acquire(self):
         '''connect to EPICS and create the HDF5 file and structure'''
         # connect to EPICS PVs
-        for pv_spec in pv_registry.values():
+        for pv_spec in self.mgr.pv_registry.values():
             pv_spec.pv = epics.PV(pv_spec.pvname)
 
         # create the file
-        for key, xture in sorted(group_registry.items()):
+        for key, xture in sorted(self.mgr.group_registry.items()):
             if key == '/':
                 # create the file and internal structure
                 f = h5py.File(self.hdf5_file_name, "w")
@@ -436,7 +217,7 @@ class SaveFlyScan(object):
                 xture.hdf5_group.attrs["NX_class"] = xture.nx_class
             addAttributes(xture.hdf5_group, **xture.attrib)
 
-        for field in field_registry.values():
+        for field in self.mgr.field_registry.values():
             if isinstance(field.text, type(u"unicode")):
                 field.text = field.text.encode('utf8')
             try:
@@ -452,6 +233,7 @@ class SaveFlyScan(object):
     def _attachEpicsAttributes(self, node, pv):
         '''attach common attributes from EPICS to the HDF5 tree node'''
         pvname = os.path.splitext(pv.pvname)[0]
+        # TODO: Add .DESC field to EpicsSignal
         desc = epics.caget(pvname+'.DESC') or ''
         addAttributes(node, **dict(
             epics_pv = pv.pvname.encode('utf8'),
