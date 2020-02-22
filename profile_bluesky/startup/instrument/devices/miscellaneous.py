e9e40867a31b40b648beec3e441ce19abe5ddacd
Pete Jemian <prjemian@gmail.com>
2020-02-21 18:19:28 -0600

    MNT #332 various

--- profile_bluesky/startup/instrument/devices/miscellaneous.py
+++ profile_bluesky/startup/instrument/devices/miscellaneous.py
@@ -5,6 +5,7 @@ miscellaneous signals and other
 
 __all__ = [
     'camy',
+    'fuel_spray_bit',
     'tcam',
     'tension',
     'waxsx',
@@ -13,12 +14,17 @@ __all__ = [
 from ..session_logs import logger
 logger.info(__file__)
 
+from ophyd import EpicsSignal
 from .usaxs_motor import UsaxsMotor
 
-camy = UsaxsMotor('9idcLAX:m58:c1:m7', name='camy')
-tcam = UsaxsMotor('9idcLAX:m58:c1:m6', name='tcam')
-tension = UsaxsMotor('9idcLAX:m58:c1:m8', name='tens')
+camy = UsaxsMotor('9idcLAX:m58:c1:m7', name='camy', labels=("motor"))
+tcam = UsaxsMotor('9idcLAX:m58:c1:m6', name='tcam', labels=("motor"))
+tension = UsaxsMotor('9idcLAX:m58:c1:m8', name='tens', labels=("motor"))
 waxsx = UsaxsMotor(
     '9idcLAX:m58:c0:m4', 
     name='waxsx', 
-    labels=("waxs",))  # WAXS X
+    labels=("waxs", "motor"))  # WAXS X
+
+fuel_spray_bit = EpicsSignal(
+    "9idcLAX:bit1", 
+    name="fuel_spray_bit")
