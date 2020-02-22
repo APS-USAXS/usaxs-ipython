13847dd55cd5764b4534541885b6710048a5231d
Pete Jemian <prjemian@gmail.com>
2020-02-21 18:51:34 -0600

    WIP #332 TODO devices

--- profile_bluesky/startup/instrument/devices/__init__.py
+++ profile_bluesky/startup/instrument/devices/__init__.py
@@ -14,3 +14,15 @@ from .scaler import *
 from .slits import *
 from .stages import *
 from .struck3820 import *
+
+# TODO:
+# 21-filters_shutters.py
+# 21-signals.py
+# 24-detector_data_file_names.py
+# 24-overrides_hdf5.py
+# 25-alta.py
+# 25-blackfly.py
+# 25-pilatus.py
+# 25-simdetector.py
+# 28-protection_plc.py
+# 29-axis_tuning.py
