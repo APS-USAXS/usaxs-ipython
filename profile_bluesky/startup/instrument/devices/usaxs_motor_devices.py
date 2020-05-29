bf21b116c315ae1e38c83beec481bb5534ab3c04
Pete Jemian <prjemian@gmail.com>
2020-05-29 16:34:10 -0500

    MNT now part of EpicsMotor

--- profile_bluesky/startup/instrument/devices/usaxs_motor_devices.py
+++ profile_bluesky/startup/instrument/devices/usaxs_motor_devices.py
@@ -12,10 +12,10 @@ from ..session_logs import logger
 logger.info(__file__)
 
 from apstools.devices import AxisTunerMixin
-from apstools.devices import EpicsMotorLimitsMixin
 from ophyd import Component, EpicsMotor, Signal
 
-class UsaxsMotor(EpicsMotorLimitsMixin, EpicsMotor): ...
+# custom for any overrides (none now)
+class UsaxsMotor(EpicsMotor): ...
 
 class UsaxsMotorTunable(AxisTunerMixin, UsaxsMotor):
     width = Component(Signal, value=0)
