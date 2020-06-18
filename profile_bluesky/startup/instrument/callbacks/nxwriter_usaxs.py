b6dd444e281a1461a0ab6effd66e8f70cd985c47
Pete Jemian <prjemian@gmail.com>
2020-06-18 14:40:10 -0500

    DOC #348

--- profile_bluesky/startup/instrument/callbacks/nxwriter_usaxs.py
+++ profile_bluesky/startup/instrument/callbacks/nxwriter_usaxs.py
@@ -6,9 +6,9 @@ define a custom NeXus file writer base for uascan raw data files
 # TODO: needs a newFile function, similar to instrument.framework.callbacks.newSpecFile()
 
 __all__ = [
-    # "NXWriterFlyScan",
+    # "NXWriterFlyScan",    # not yet tested
     "NXWriterUascan",
-    # "NXWriterSaxsWaxs",
+    # "NXWriterSaxsWaxs",    # not yet tested
     ]
 
 # from ..session_logs import logger
@@ -21,6 +21,37 @@ import os
 
 
 class OurCustomNXWriterBase(NXWriterAps):
+    """
+    customize the NXWriter for this instrument
+
+    Structure of the NeXus/HDF5 file
+
+    The HDF5 files are written according to the NeXus structure, 
+    using only NeXus base classes.  Here is a tree view of
+    the structure, edited from an example uascan run::
+
+        entry:NXentry
+            # constants and links into bluesky subdirectories
+            instrument:NXinstrument
+                bluesky_metadata:NXnote
+                    # each key in the ``start`` document
+                    # structured data encoded here as YAML
+                bluesky_streams:NXnote
+                    # NXdata groups, one for each monitored signal
+                monochromator:NXmonochromator
+                    # links to baseline start values
+                slits:NXnote
+                    guard_slit:NXslit
+                        # links to baseline start values
+                    usaxs_slit:NXslit
+                        # links to baseline start values
+                source:NXsource
+                    # constants and links to baseline start values
+                undulator:NXinsertion_device
+                    # constants and links to baseline start values
+            sample:NXsample
+                # links to metadata and baseline start values
+    """
 
     instrument_name = 'APS 9-ID-C USAXS'
     supported_plans = ("name", "the", "supported", "plans")
@@ -98,84 +129,6 @@ class NXWriterSaxsWaxs(OurCustomNXWriterBase):
 class NXWriterUascan(OurCustomNXWriterBase):
     """
     write raw uascan data to a NeXus/HDF5 file, no specific application definition
-
-    Structure of the NeXus/HDF5 file
-
-    The HDF5 files are written according to the NeXus structure, 
-    using only NeXus base classes.  Here is a tree view of
-    the structure, edited from an example uascan run::
-
-        entry:NXentry
-            duration:NX_FLOAT64[] = [ ... ]
-            end_time:NX_CHAR = 2020-06-10T13:55:39.559547
-            entry_identifier:NX_CHAR = 78b4801c-1db1-4674-b894-03c4ef4d12a5
-            program_name:NX_CHAR = bluesky
-            run_cycle --> /entry/instrument/bluesky_streams/baseline/aps_aps_cycle/value_start
-            start_time:NX_CHAR = 2020-06-10T13:50:50.239672
-            title --> /entry/instrument/bluesky_streams/baseline/user_data_sample_title/value_start
-            contact:NXuser
-            role:NX_CHAR = contact
-            # links to data in /entry/instrument/bluesky_streams/baseline/*/value_start
-            data:NXdata
-            EPOCH --> /entry/instrument/bluesky_streams/primary/upd_autorange_controls_reqrange/time
-            # links to each signal in the primary stream
-            I00_autorange_controls_gain --> /entry/instrument/bluesky_streams/primary/I00_autorange_controls_gain/value
-            # ...
-            instrument:NXinstrument
-            bluesky_metadata:NXnote
-                # each key in the ``start`` document
-                # structured data encoded here as YAML
-                EPICS_CA_MAX_ARRAY_BYTES:NX_CHAR = b'1280000'
-                ay0:NX_FLOAT64[] = [ ... ]
-                # ...
-            bluesky_streams:NXnote
-                # NXdata groups, one for each monitored signal
-                aps_current_monitor:NXnote
-                aps_current:NXdata
-                    EPOCH:NX_FLOAT64[575] = [ ... ]
-                    time:NX_FLOAT64[575] = [ ... ]
-                    value:NX_FLOAT64[575] = [ ... ]
-                baseline:NXnote
-                # NXdata groups, one for each signal in baseline
-                aps_aps_cycle:NXdata
-                    EPOCH:NX_FLOAT64[2] = [ ... ]
-                    time:NX_FLOAT64[2] = [ ... ]
-                    value:NX_CHAR[6] = 2020-2
-                    value_end:NX_CHAR = b'2020-2'
-                    value_start:NX_CHAR = b'2020-2'
-                aps_current:NXdata
-                    EPOCH:NX_FLOAT64[2] = [ ... ]
-                    time:NX_FLOAT64[2] = [ ... ]
-                    value:NX_FLOAT64[2] = [ ... ]
-                    value_end:NX_FLOAT64[] = [ ... ]
-                    value_start:NX_FLOAT64[] = [ ... ]
-                # ...
-                primary:NXnote
-                # NXdata groups, one for each signal hinted in the plan
-                I00_autorange_controls_gain:NXdata
-                    EPOCH:NX_FLOAT64[300] = [ ... ]     # absolute time stamps
-                    time:NX_FLOAT64[300] = [ ... ]      # EPOCH - EPOCH[0]
-                    value:NX_FLOAT64[300] = [ ... ]     # values
-                # ...
-            monochromator:NXmonochromator
-                # links to data in /entry/instrument/bluesky_streams/baseline/*/value_start
-            slits:NXnote
-                guard_slit:NXslit
-                # links to data in /entry/instrument/bluesky_streams/baseline/*/value_start
-                usaxs_slit:NXslit
-                # links to data in /entry/instrument/bluesky_streams/baseline/*/value_start
-            source:NXsource
-                energy:NX_INT64[] = [ ... ]
-                name:NX_CHAR = Advanced Photon Source
-                probe:NX_CHAR = x-ray
-                type:NX_CHAR = Synchrotron X-ray Source
-                # links to data in /entry/instrument/bluesky_streams/baseline/*/value_start
-            undulator:NXinsertion_device
-                type:NX_CHAR = undulator
-                # links to data in /entry/instrument/bluesky_streams/baseline/*/value_start
-            sample:NXsample
-            # links to data in /entry/instrument/bluesky_streams/baseline/*/value_start
-
     """
     # TODO: identify what additional data is needed to collect
     # add RE.md["detectors"] = list : first item is for NXdata @signal attribute
