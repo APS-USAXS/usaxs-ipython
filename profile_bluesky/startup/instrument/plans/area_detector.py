f2c98231c56147e02bcdee4f2d8b00a2dc457ee1
Pete Jemian <prjemian@gmail.com>
2020-07-22 16:30:30 -0500

    MNT #274 standardize md handling

--- profile_bluesky/startup/instrument/plans/area_detector.py
+++ profile_bluesky/startup/instrument/plans/area_detector.py
@@ -19,10 +19,11 @@ from ..framework import RE
 from ..utils.reporter import remaining_time_reporter
 
 
-def areaDetectorAcquire(det, md={}):
+def areaDetectorAcquire(det, md=None):
     """
     acquire image(s) from the named area detector
     """
+    _md = md or {}
     acquire_time = det.cam.acquire_time.get()
     # Note: AD's HDF File Writer can use up to 5 seconds to finish writing the file
     
@@ -31,15 +32,15 @@ def areaDetectorAcquire(det, md={}):
         user_data.scanning, "scanning",          # we are scanning now (or will be very soon)
     )
     logger.debug(f"areaDetectorAcquire(): {det.hdf1.stage_sigs}")
-    md["method"] = "areaDetectorAcquire"
-    md["area_detector_name"] = det.name
-    if md.get("plan_name") is None:
-        md["plan_name"] = "image"
+    _md["method"] = "areaDetectorAcquire"
+    _md["area_detector_name"] = det.name
+    if _md.get("plan_name") is None:
+        _md["plan_name"] = "image"
 
     if RE.state != "idle":
-        remaining_time_reporter(md["plan_name"], acquire_time)
+        remaining_time_reporter(_md["plan_name"], acquire_time)
 
-    yield from bp.count([det], md=md)          # TODO: SPEC showed users incremental progress (1 Hz updates) #175
+    yield from bp.count([det], md=_md)          # TODO: SPEC showed users incremental progress (1 Hz updates) #175
 
     yield from bps.mv(user_data.scanning, "no",)  # we are done
     elapsed = time.time() - t0
