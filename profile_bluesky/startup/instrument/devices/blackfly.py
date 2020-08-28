6b710068f14cf68c97e639cf06944172e5e2ee37
Pete Jemian <prjemian@gmail.com>
2020-08-28 15:15:27 -0500

    MNT #373 integrate plugins

--- profile_bluesky/startup/instrument/devices/blackfly.py
+++ profile_bluesky/startup/instrument/devices/blackfly.py
@@ -6,9 +6,9 @@ note: this is one of the easiest area detector setups in Ophyd
 """
 
 __all__ = [
-    'blackfly_det', 
-    'blackfly_optical', 
-    'blackfly_radiography', 
+    'blackfly_det',
+    'blackfly_optical',
+    'blackfly_radiography',
     ]
 
 from ..session_logs import logger
@@ -19,27 +19,58 @@ from ophyd import PointGreyDetectorCam
 from ophyd import SingleTrigger, ImagePlugin
 from ophyd.areadetector import ADComponent
 
+from .area_detector_common import _validate_AD_FileWriter_path_
 from .area_detector_common import area_detector_EPICS_PV_prefix
-from .area_detector_common import _validate_AD_HDF5_path_
+from .area_detector_common import DATABROKER_ROOT_PATH
+from .area_detector_common import EpicsDefinesHDF5FileNames
+from .area_detector_common import EpicsDefinesJpegFileNames
 
 
-ORIGINAL_CAMERA = 'PointGrey BlackFly'
-OPTICAL_CAMERA = 'PointGrey BlackFly Optical'
-RADIOGRAPHY_CAMERA = 'PointGrey BlackFly Radiography'
+ORIGINAL_CAMERA = 'PointGrey BlackFly'                      # 9idFLY1:
+OPTICAL_CAMERA = 'PointGrey BlackFly Optical'               # 9idFLY2:
+RADIOGRAPHY_CAMERA = 'PointGrey BlackFly Radiography'       # 9idFLY:
+
+
+# path for image files (as seen by EPICS area detector writer plugin)
+# path seen by detector IOC
+PATH_BASE = "/share1/USAXS_data/test/blackfly_optical"
+WRITE_IMAGE_FILE_PATH = PATH_BASE + "/%Y/%m/%d/"
+# path seen by databroker
+READ_IMAGE_FILE_PATH = WRITE_IMAGE_FILE_PATH
+
+_validate_AD_FileWriter_path_(WRITE_IMAGE_FILE_PATH, DATABROKER_ROOT_PATH)
+_validate_AD_FileWriter_path_(READ_IMAGE_FILE_PATH, DATABROKER_ROOT_PATH)
 
 
 class MyPointGreyDetector(SingleTrigger, AreaDetector):
     """PointGrey Black Fly detector(s) as used by 9-ID-C USAXS"""
-    
+
     cam = ADComponent(PointGreyDetectorCam, "cam1:")
     image = ADComponent(ImagePlugin, "image1:")
 
 
+class MyPointGreyDetectorHDF5(MyPointGreyDetector):
+    """Variation to write image as HDF5"""
+
+    hdf1 = ADComponent(
+        EpicsDefinesHDF5FileNames,
+        suffix = "HDF1:",
+        root = DATABROKER_ROOT_PATH,
+        write_path_template = WRITE_IMAGE_FILE_PATH,
+        read_path_template = READ_IMAGE_FILE_PATH,
+        )
+
+
 class MyPointGreyDetectorJPEG(MyPointGreyDetector):
     """Variation to write image as JPEG"""
-    
-    # jpeg = ADComponent(ImagePlugin, "image1:")
-    pass
+
+    jpeg = ADComponent(
+        EpicsDefinesJpegFileNames,
+        suffix = "JPEG1:",
+        root = DATABROKER_ROOT_PATH,
+        write_path_template = WRITE_IMAGE_FILE_PATH,
+        read_path_template = READ_IMAGE_FILE_PATH,
+        )
 
 
 try:
@@ -69,7 +100,7 @@ except TimeoutError as exc_obj:
 try:
     nm = RADIOGRAPHY_CAMERA
     prefix = area_detector_EPICS_PV_prefix[nm]
-    blackfly_radiography = MyPointGreyDetector(
+    blackfly_radiography = MyPointGreyDetectorHDF5(
         prefix, name="blackfly_radiography",
         labels=["camera", "area_detector"])
 except TimeoutError as exc_obj:
