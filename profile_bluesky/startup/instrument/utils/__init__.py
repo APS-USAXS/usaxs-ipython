f881d80e22bae27249dabafd012bbb84ac0325da
Pete Jemian <prjemian@gmail.com>
2020-09-08 15:02:21 -0500

    MNT #379 after test at beamline

--- profile_bluesky/startup/instrument/utils/__init__.py
+++ profile_bluesky/startup/instrument/utils/__init__.py
@@ -9,7 +9,6 @@ not bluesky plans (no need to execute in RE() wrapper)
 #-------------
 from .motors import *
 
-
 # called by other code
 # (no need to import into global namespace)
 #------------------------------------------
@@ -21,3 +20,4 @@ from .motors import *
 # from .plot_prune import *
 # from .reporter import *
 # from .quoted_line import *
+# from .setup_new_user import *
