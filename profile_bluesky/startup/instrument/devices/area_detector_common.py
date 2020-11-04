f3b1b6296b336b913aeb92461b526b578827196d
Pete Jemian <prjemian@gmail.com>
2020-11-04 12:26:42 -0600

    fixes #407

--- profile_bluesky/startup/instrument/devices/area_detector_common.py
+++ profile_bluesky/startup/instrument/devices/area_detector_common.py
@@ -13,20 +13,25 @@ file systems on some area detectors need more work
 * Alta: /mnt/share1/USAXS_data/...
 """
 
-__all__ = [
-    'area_detector_EPICS_PV_prefix',
-    'DATABROKER_ROOT_PATH',
-    'EpicsDefinesHDF5FileNames',
-    'myHdf5EpicsIterativeWriter',
-    'myHDF5FileNames',
-    '_validate_AD_FileWriter_path_',
-    ]
+__all__ = """
+    _validate_AD_FileWriter_path_
+    area_detector_EPICS_PV_prefix
+    DATABROKER_ROOT_PATH
+    EpicsDefinesHDF5FileNames
+    EpicsDefinesJpegFileNames
+    myHdf5EpicsIterativeWriter
+    myHDF5FileNames
+    myJpegEpicsIterativeWriter
+    myJpegFileNames
+    """.split()
+
+
 
 from ..session_logs import logger
 logger.info(__file__)
 
-from .for_apstools import AD_EpicsJpegFileName
 from apstools.devices import AD_EpicsHdf5FileName
+from apstools.devices import AD_EpicsJpegFileName
 from ophyd import HDF5Plugin
 from ophyd import JPEGPlugin
 from ophyd.areadetector.filestore_mixins import FileStoreIterativeWrite
@@ -51,12 +56,14 @@ def _validate_AD_FileWriter_path_(path, root_path):
             f"  path '{path}' must start with '{root_path}"
         ))
 
-# custom support is in AD_EpicsHdf5FileName
-class myHdf5EpicsIterativeWriter(AD_EpicsHdf5FileName, FileStoreIterativeWrite): ...
+class myHdf5EpicsIterativeWriter(AD_EpicsHdf5FileName,
+                                 FileStoreIterativeWrite): ...
 class myHDF5FileNames(HDF5Plugin, myHdf5EpicsIterativeWriter): ...
-class EpicsDefinesHDF5FileNames(HDF5Plugin, myHdf5EpicsIterativeWriter): ...
+class EpicsDefinesHDF5FileNames(HDF5Plugin,
+                                myHdf5EpicsIterativeWriter): ...
 
-# custom support is in AD_EpicsJpegFileName
-class myJpegEpicsIterativeWriter(AD_EpicsJpegFileName, FileStoreIterativeWrite): ...
+class myJpegEpicsIterativeWriter(AD_EpicsJpegFileName,
+                                 FileStoreIterativeWrite): ...
 class myJpegFileNames(JPEGPlugin, myJpegEpicsIterativeWriter): ...
-class EpicsDefinesJpegFileNames(JPEGPlugin, myJpegEpicsIterativeWriter): ...
+class EpicsDefinesJpegFileNames(JPEGPlugin,
+                                myJpegEpicsIterativeWriter): ...
