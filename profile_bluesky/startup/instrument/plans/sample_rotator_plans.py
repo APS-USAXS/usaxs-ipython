b838e66b9f2301766e5dc17a3a0b869a091cbfc8
Pete Jemian <prjemian@gmail.com>
2020-11-18 13:32:11 -0600

    MNT #432

--- profile_bluesky/startup/instrument/plans/sample_rotator_plans.py
+++ profile_bluesky/startup/instrument/plans/sample_rotator_plans.py
@@ -16,16 +16,16 @@ from ..devices import pi_c867
 from bluesky import plan_stubs as bps
 
 
-def PI_Off(timeout=1):
+def PI_Off(timeout=1, md=None):
     """plan: stop rotating sample in either direction"""
     yield from bps.abs_set(pi_c867.motor_stop, 1, timeout=timeout)
 
-def PI_onF(timeout=10):
+def PI_onF(timeout=10, md=None):
     """plan: start rotating sample in forward direction"""
     yield from bps.mv(pi_c867.home, "forward", timeout=timeout)
     yield from bps.abs_set(pi_c867.jog_forward, 1)
 
-def PI_onR(timeout=10):
+def PI_onR(timeout=10, md=None):
     """plan: start rotating sample in reverse direction"""
     yield from bps.mv(pi_c867.home, "reverse", timeout=timeout)
     yield from bps.abs_set(pi_c867.jog_reverse, 1)
