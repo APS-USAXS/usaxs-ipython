f3e2e022644af6abb4cdf7ed77b3c38f72e5e794
Pete Jemian <prjemian@gmail.com>
2020-11-18 06:45:06 -0600

    MNT #429 no auto_monitor on Autosave.disable signal

--- profile_bluesky/startup/instrument/devices/autosave.py
+++ profile_bluesky/startup/instrument/devices/autosave.py
@@ -15,7 +15,7 @@ from ophyd import Component, Device, EpicsSignal
 
 class Autosave(Device):
     """control of autosave routine in EPICS IOC"""
-    disable = Component(EpicsSignal, "SR_disable")
+    disable = Component(EpicsSignal, "SR_disable", auto_monitor=False)
     max_time = Component(EpicsSignal, "SR_disableMaxSecs")
 
 # autosave on LAX
