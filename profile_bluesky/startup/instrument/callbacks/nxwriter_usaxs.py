b8b979eba374754134bef39caac58ccffd86040c
Pete Jemian <prjemian@gmail.com>
2020-11-19 15:48:42 -0600

    LGTM remove unnecessary assignment

--- profile_bluesky/startup/instrument/callbacks/nxwriter_usaxs.py
+++ profile_bluesky/startup/instrument/callbacks/nxwriter_usaxs.py
@@ -197,7 +197,6 @@ class NXWriterUascan(OurCustomNXWriterBase):
         group: /entry/instrument/slits:NXnote/SLIT:NXslit
         """
         group = self.create_NX_group(parent, f"slits:NXnote")
-        pre = "guard_slit"
         for pre in "guard_slit usaxs_slit".split():
             slit = self.create_NX_group(group, f"{pre}:NXslit")
             slit["x_gap"] = self.get_stream_link(f"{pre}_h_size")
