12fd744730aa606face0ccd491ea5d3a5096ee34
Pete Jemian <prjemian@gmail.com>
2021-01-19 10:59:47 -0600

    MNT #471 rename: avoid command line name confusion

--- profile_bluesky/startup/instrument/devices/__init__.py
+++ profile_bluesky/startup/instrument/devices/__init__.py
@@ -34,10 +34,10 @@ from .trajectories import *
 from .usaxs_fly_scan import *
 
 # finally these area detectors
-from .alta import *
-from .blackfly import *
-from .dexela import *
-from .pilatus import *
+from .alta_module import *
+from .blackfly_module import *
+from .dexela_module import *
+from .pilatus_module import *
 # from .simdetector import *
 
 # and only when all devices are defined
