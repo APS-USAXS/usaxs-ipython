98910f9bf67758ed50eb3fd2c030108055f8dc34
Jan Ilavsky <jilavsky@gmail.com>
2021-07-14 15:51:13 -0500

    Update laser.py
    Fixes to make more readable description

--- profile_bluesky/startup/instrument/devices/laser.py
+++ profile_bluesky/startup/instrument/devices/laser.py
@@ -17,13 +17,15 @@ from ophyd import Component, Device, EpicsSignal, EpicsSignalRO
 
 class LaserAR500(Device):
     """
-    temporary support for laser distacne meter AR500
-    uses analog in and digital out from Galil
+    Temporary support for laser distacne meter AR500
+    https://www.acuitylaser.com/product/laser-sensors/short-range-sensors/ar500-laser-position-sensor/
+    uses Galil analog in and digital out to read distacne as analog volatge and switch AR500 on/off 
+    uses Galil userAve (2 seconds long) to average noise
     uses userCalc7 in LAX to convert to real distace
-    uses Galil userAve (2 seconds long) to average noise out.
     to read distace use laser.distance.get()
-    laser.enable(1)
-    laser.enable(0)
+    to switch on use laser.enable(1)
+    to switch off use laser.enable(0)
+    dx_in and dy_in reads defined positions from epics. PVs and on Parameters GUI. 
     """
     distance    = Component(EpicsSignalRO, "9idcLAX:userCalc7.VAL")
     enable      = Component(EpicsSignal, "9idcRIO:Galil2Bo1_CMD")
